7f889f40ced3a0c6d5d8d7aa92933e464a3b935c
sfox123 <shagyfox133@gmail.com>
2023-08-30 15:18:07 +0530

    initial commit

---
+++ src/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav className="flex bg-gray-300 w-full justify-between p-2">
+      <div className="rounded-full w-10 h-10 bg-gray-600 ml-auto"></div>
+    </nav>
+  );
+}
